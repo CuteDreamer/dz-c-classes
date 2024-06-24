59908beb98f9ac273430f5d71cf33d786c0e08b1
Artem Karp <moment.of.peaces@gmail.com>
2024-06-24 21:49:29 +0300

    Добавил конструкторы

--- dz c# classes/Aspirant.cs
+++ dz c# classes/Aspirant.cs
@@ -11,6 +11,35 @@ namespace dz_c__classes
         private string? ThesisTheme;
 
 
+        public Aspirant() : base()
+        {
+            SetThesisTheme("Тема диссертации по умолчанию");
+        }
+
+        public Aspirant(string? name) : base(name)
+        {
+            SetThesisTheme("Тема диссертации по умолчанию");
+        }
+
+        public Aspirant(string? name, string? lastname) : base(name, lastname)
+        {
+            SetThesisTheme("Тема диссертации по умолчанию");
+        }
+
+        public Aspirant(string? name, string? lastname, string? patron) : base(name, lastname, patron)
+        {
+            SetThesisTheme("Тема диссертации по умолчанию");
+        }
+
+        public Aspirant(string? name, string? lastname, string? patron, string? date, string? address, string? phone, string? ThesisTheme) : base(name, lastname, patron, date, address, phone)
+        {
+            SetThesisTheme(ThesisTheme);
+        }
+
+        public void SetThesisTheme(string? ThesisTheme)
+        {
+            this.ThesisTheme = ThesisTheme;
+        }
         public void DoInternShip()
         {
             Console.WriteLine("ШОСЬ ВИДБУВАЕТЬСЯ");
